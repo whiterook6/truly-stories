4586836cfe4e600c83ebda85ec63e2e8fa51bcc6
Tim Graboski <whiterook6@gmail.com>
2026-08-12 11:31:19 -0700

    adding sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fiction.graboski.ca',
+
   build: {
     inlineStylesheets: 'always',
   },
+
   server: {
     port: 5173,
   },
+
+  integrations: [
+    sitemap({
+      filter: (page) => {
+        const pathname = new URL(page).pathname;
+        return pathname !== '/share' && !pathname.startsWith('/share/');
+      },
+    }),
+  ],
 });
